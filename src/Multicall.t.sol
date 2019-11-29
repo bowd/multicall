edb5e535c001991bfbda52982829bca752ce39f8
Bogdan Dumitru <dumbogdan@gmail.com>
2019-11-29 12:15:09 +0200

    Modify aggregate call to support a stirct mode toggle

--- src/Multicall.t.sol
+++ src/Multicall.t.sol
@@ -10,7 +10,12 @@ contract Store {
     function get() public view returns (uint256) { return val; }
     function getAnd10() public view returns (uint256, uint256) { return (val, 10); }
     function getAdd(uint256 _val) public view returns (uint256) { return val + _val; }
+    function getFailing(uint256 _val) public view returns (uint256) {
+      require(false);
+      return val + _val;
+    } 
 }
+
 
 // We inherit from Multicall rather than deploy an instance because solidity
 // can't return dynamically sized byte arrays from external contracts
@@ -39,13 +44,15 @@ contract MulticallTest is DSTest, Multicall {
         _calls[0].target = address(storeA);
         _calls[0].callData = abi.encodeWithSignature("get()");
 
-        (, bytes[] memory _returnData) = aggregate(_calls);
+        (, Return[] memory _returnData) = aggregate(_calls, true);
 
-        bytes memory _word = _returnData[0];
+        bytes memory _word = _returnData[0].data;
+        bool _success = _returnData[0].success;
         uint256 _returnVal;
         assembly { _returnVal := mload(add(0x20, _word)) }
 
         assertEq(_returnVal, 123);
+        assert(_success);
     }
 
     function test_multi_call_single_return_no_args() public {
@@ -58,10 +65,12 @@ contract MulticallTest is DSTest, Multicall {
         _calls[1].target = address(storeB);
         _calls[1].callData = abi.encodeWithSignature("get()");
 
-        (, bytes[] memory _returnData) = aggregate(_calls);
+        (, Return[] memory _returnData) = aggregate(_calls, true);
 
-        bytes memory _wordA = _returnData[0];
-        bytes memory _wordB = _returnData[1];
+        bool _successA = _returnData[0].success;
+        bool _successB = _returnData[1].success;
+        bytes memory _wordA = _returnData[0].data;
+        bytes memory _wordB = _returnData[1].data;
         uint256 _returnValA;
         uint256 _returnValB;
         assembly { _returnValA := mload(add(0x20, _wordA)) }
@@ -69,6 +78,8 @@ contract MulticallTest is DSTest, Multicall {
 
         assertEq(_returnValA, 123);
         assertEq(_returnValB, 321);
+        assert(_successA);
+        assert(_successB);
     }
 
     function test_single_call_single_return_single_arg() public {
@@ -78,13 +89,15 @@ contract MulticallTest is DSTest, Multicall {
         _calls[0].target = address(storeA);
         _calls[0].callData = abi.encodeWithSignature("getAdd(uint256)", 1);
 
-        (, bytes[] memory _returnData) = aggregate(_calls);
+        (, Return[] memory _returnData) = aggregate(_calls, true);
 
-        bytes memory _word = _returnData[0];
+        bytes memory _word = _returnData[0].data;
+        bool _success = _returnData[0].success;
         uint256 _returnVal;
         assembly { _returnVal := mload(add(0x20, _word)) }
 
         assertEq(_returnVal, 124);
+        assert(_success);
     }
 
     function test_multi_call_single_return_single_arg() public {
@@ -97,10 +110,12 @@ contract MulticallTest is DSTest, Multicall {
         _calls[1].target = address(storeB);
         _calls[1].callData = abi.encodeWithSignature("getAdd(uint256)", 1);
 
-        (, bytes[] memory _returnData) = aggregate(_calls);
+        (, Return[] memory _returnData) = aggregate(_calls, true);
 
-        bytes memory _wordA = _returnData[0];
-        bytes memory _wordB = _returnData[1];
+        bool _successA = _returnData[0].success;
+        bool _successB = _returnData[1].success;
+        bytes memory _wordA = _returnData[0].data;
+        bytes memory _wordB = _returnData[1].data;
         uint256 _returnValA;
         uint256 _returnValB;
         assembly { _returnValA := mload(add(0x20, _wordA)) }
@@ -108,6 +123,8 @@ contract MulticallTest is DSTest, Multicall {
 
         assertEq(_returnValA, 124);
         assertEq(_returnValB, 322);
+        assert(_successA);
+        assert(_successB);
     }
 
     function test_single_call_multi_return_no_args() public {
@@ -117,9 +134,10 @@ contract MulticallTest is DSTest, Multicall {
         _calls[0].target = address(storeA);
         _calls[0].callData = abi.encodeWithSignature("getAnd10()");
 
-        (, bytes[] memory _returnData) = aggregate(_calls);
+        (, Return[] memory _returnData) = aggregate(_calls, true);
 
-        bytes memory _words = _returnData[0];
+        bool _success = _returnData[0].success;
+        bytes memory _words = _returnData[0].data;
         uint256 _returnValA1;
         uint256 _returnValA2;
         assembly { _returnValA1 := mload(add(0x20, _words)) }
@@ -127,7 +145,59 @@ contract MulticallTest is DSTest, Multicall {
 
         assertEq(_returnValA1, 123);
         assertEq(_returnValA2, 10);
+        assert(_success);
+    }
 
+    function testFail_single_call_with_failure_in_strict_mode() public {
+        storeA.set(123);
+
+        Call[] memory _calls = new Call[](1);
+        _calls[0].target = address(storeA);
+        _calls[0].callData = abi.encodeWithSignature("getFailing()");
+
+        aggregate(_calls, true);
+    }
+
+    function test_single_call_with_failure() public {
+        storeA.set(123);
+
+        Call[] memory _calls = new Call[](1);
+        _calls[0].target = address(storeA);
+        _calls[0].callData = abi.encodeWithSignature("getFailing()");
+
+        (, Return[] memory _returnData) = aggregate(_calls, false);
+
+        bool _success = _returnData[0].success;
+        bytes memory _words = _returnData[0].data;
+
+        assert(_success == false);
+        assertEq0(_words, "");
+    }
+
+    function test_multi_call_with_one_failure() public {
+        storeA.set(123);
+        storeB.set(123);
+
+        Call[] memory _calls = new Call[](2);
+        _calls[0].target = address(storeA);
+        _calls[0].callData = abi.encodeWithSignature("getFailing()");
+        _calls[1].target = address(storeB);
+        _calls[1].callData = abi.encodeWithSignature("getAdd(uint256)", 1);
+
+        (, Return[] memory _returnData) = aggregate(_calls, false);
+
+        bool _successA = _returnData[0].success;
+        bytes memory _wordA = _returnData[0].data;
+
+        bool _successB = _returnData[1].success;
+        bytes memory _wordB = _returnData[1].data;
+        uint256 _returnValB;
+        assembly { _returnValB := mload(add(0x20, _wordB)) }
+
+        assert(_successA == false);
+        assert(_successB);
+        assertEq0(_wordA, "");
+        assertEq(_returnValB, 124);
     }
 
     function test_helpers() public {
